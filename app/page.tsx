f94ddb0ef8d6000a5c14b59fde39bdda8f532983
sharmadotexe <rockvikas300@gmail.com>
2024-06-11 14:02:59 +0530

    Updated Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Hero from "@/components/hero";
+
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Portfolio</h1>
+        <Hero/>
       </div>
-    </main>
+    </main> 
   );
 }
