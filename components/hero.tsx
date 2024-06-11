b314c3236509eb844f3c412fd5520bc29b437556
sharmadotexe <rockvikas300@gmail.com>
2024-06-11 14:26:10 +0530

    Hero section completed

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,3 +1,4 @@
+import { FaLocationArrow } from 'react-icons/fa'
 import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
@@ -33,6 +34,8 @@ const Hero = () => {
           <a href='#about'> 
             <MagicButton 
             title='Show My Work'
+            icon={<FaLocationArrow/>}
+            position='right'
             />
           </a>
 
