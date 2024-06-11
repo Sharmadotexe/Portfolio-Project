f8291a3ad9cac2dbe4ba154e49c7b5a2da303a6c
sharmadotexe <rockvikas300@gmail.com>
2024-06-11 21:14:03 +0530

    Bento Grid Section started

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export const gridItems = [
+    {  title:'title1', description:'Desc1', id:1},
+    {  title:'title1', description:'Desc1', id:1},
+    {  title:'title1', description:'Desc1', id:1},
+    {  title:'title1', description:'Desc1', id:1},
+    {  title:'title1', description:'Desc1', id:1},
+    {  title:'title1', description:'Desc1', id:1},
+]
