f68e820837c480644b64983e19865dc2f60cca9f
sharmadotexe <rockvikas300@gmail.com>
2024-06-14 02:14:36 +0530

    Project section completed & interest section started

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Approach from "@/components/Approach";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Hero from "@/components/hero";
 import { FloatingNav } from "@/components/ui/Navbar";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 
@@ -10,12 +12,11 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome/>}
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero/>
         <Grid/>
         <RecentProjects/>
+        <Approach/>
       </div>
     </main> 
   );  
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,19 +1,20 @@
 import { projects } from '@/data'
 import React from 'react'
 import { PinContainer } from './ui/3d-pin'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
   return (
-    <div className='py-20' >
+    <div className='py-20' id='projects' >
         <h1 className='heading'>
             A Small Selection of {' '}
             <span className='text-purple'>Recent Projects</span>
         </h1>
-        <div className='flex flex-wrap justify-center items-center p-4 gap-16 mt-10'>
+        <div className='flex flex-wrap justify-center items-center p-4 gap-x-24 gap-y-8 mt-10'>
             {projects.map(({id, des, title, img, iconLists, link}) => (
-                <div key ={id} className='lg:min-h-[32.5rem] h-[25rem] flex items justify-center sm:w-96 w-[80vw] '>
-                    <PinContainer title={title} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                <div key ={id} className='sm:h-[41rem] sm:w-[570px] h-[32rem] lg:min-h-[32.5rem] flex items justify-center w-[80vw] '>
+                    <PinContainer title={link} href={link}>
+                        <div className='relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] overflow-hidden h-[30vh] mb-10'>
                             <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
                                 <img src='/bg.png' alt='bg-img'/>
                             </div>
@@ -30,8 +31,18 @@ const RecentProjects = () => {
                             {des}
                         </p>
 
-                        <div className=''>
-
+                        <div className='flex item items-center justify-between mt-7 mb-3'>
+                            <div className='flex items-center '>
+                                {iconLists.map((icon)=>(
+                                    <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center '> 
+                                    <img src={icon} alt={icon} className='p-2'/>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='flex justify-center items-center'>
+                                <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Github Link</p>
+                                <FaLocationArrow className='ms-3 colour="#CBACF9'/>
+                            </div>
                         </div>
                     </PinContainer>
                 </div>
